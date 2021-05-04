a2cc4137d2313196c1c3e82daa5e543da9b9d754
lukemifsud46 <lukemifsud_4@hotmail.com>
2021-05-04 09:07:55 +0200

    changes

--- src/app/meals/meals.page.ts
+++ src/app/meals/meals.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-meals',
   templateUrl: './meals.page.html',
   styleUrls: ['./meals.page.scss'],
 })
 export class MealsPage implements OnInit {
+  formattedDate;
+  constructor(
+  ) { 
+    this.getFormattedDate()
+  }
 
-  constructor() { }
+  getFormattedDate(){
+    var dateObj = new Date()
+
+    var year = dateObj.getFullYear()
+    var month = dateObj.getMonth()
+    var date = dateObj.getDate()
+
+    this.formattedDate = date +'/'+ month +'/'+ year;
+  }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
