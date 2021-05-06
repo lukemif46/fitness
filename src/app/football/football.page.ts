a8e47bdc712ea0413b8a6794cd725bec019954eb
lukemifsud46 <lukemifsud_4@hotmail.com>
2021-05-06 19:01:42 +0200

    Details continuation

--- src/app/football/football.page.ts
+++ src/app/football/football.page.ts
@@ -8,9 +8,9 @@ import { Router } from '@angular/router';
 })
 export class FootballPage implements OnInit {
   drills: any =[
-    {id: 1, title: 'Footwork'},
-    {id: 2, title: 'Dribbling'},
-    {id: 3, title: 'Shooting'},
+    {id: 1, title: 'Footwork', desc1: 'La croqueta', desc2: 'Inside-outside'},
+    {id: 2, title: 'Dribbling',desc3: 'Shuttles', desc4: 'Straight cone dribble'},
+    {id: 3, title: 'Shooting',desc5: 'Turn and shoot', desc6: 'Zone shooting'},
     {id: 4, title: 'Control'},
     {id: 5, title: 'Defending'},
     {id: 6, title: 'Attacking'},
